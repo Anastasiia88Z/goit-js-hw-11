d3b7ea74b9b67f8baf84d4cbd706f8cf53c88532
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-07-10 14:43:36 +0200

    add color switch

---
+++ src/task 1.js
@@ -0,0 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener("click", clickOnStart);
+stopBtn.addEventListener("click", clickOnStop);
+
+let isActive = false;
+function clickOnStart() {
+  if(isActive) {
+    return
+  }
+  isActive = true;
+  intervalId = setInterval(changeBodyColor, 1000,1000);
+}
+
+function changeBodyColor() {
+  console.log(document.body.style.background = `#${Math.floor(Math.random() * 16777215).toString(16)}`);
+}
+
+function clickOnStop() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
