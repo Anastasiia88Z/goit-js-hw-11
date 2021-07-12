7dd1a8524bcdd69893abd8d481cbea0dc507cce2
Anastasiia Zhylina <anastasiiazhylina@gmail.com>
2021-07-12 23:40:34 +0200

    add change

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,63 +1,87 @@
-import '../css/common.css';
+
 import "../sass/main.scss";
 
+import Swal from 'sweetalert2'
+
+const refs = {
+    choosedDate: document.querySelector('#date-selector'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+    startBtn: document.querySelector('[data-start]')
+}
+
+refs.choosedDate.addEventListener('change', dateChoosing)
+// refs.startBtn.style.display = 'none';
 
 
-class CountdownTimer {
-
-  constructor({selector, targetDate}) {
-    this.selector = selector;
-    this.targetDate = targetDate;
-    this.refs = {
-      days: document.querySelector('[data-value="days"]'),
-      hours: document.querySelector('[data-value="hours"]'),
-      mins: document.querySelector('[data-value="mins"]'),
-      secs: document.querySelector('[data-value="secs"]'),
+function dateChoosing() {
+    const input = this.value;
+    const dateEntered = new Date(input);
+    const currentDate = Date.now();
+    let time = dateEntered - currentDate;
+    if (time < 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please choose a date in the future',
+        })
+        return
+         
     }
-  
-
-    setInterval(() => {
-        const startDate = Date.now();
-        const time = this.targetDate - startDate;
-        const { days, hours, mins, secs } = this.getTimeComponents(time);
-
-        this.updateClockface(this.getTimeComponents(time));
-
-      }, 1000);
-   }
-
-// - Принимает время в миллисекундах
-// - Вычисляет сколько в них вмещается дней, часов, минут, секунд
-// - Возвращает обьект со свойствами days, hours, mins, secs
-   getTimeComponents(time) {
-  
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  
-    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    
+//  refs.startBtn.style.display = 'block';
    
-    const mins =  this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      
-    const secs =  this.pad(Math.floor((time % (1000 * 60)) / 1000));
-  
-      return { days, hours, mins, secs };
-  }
 
-// Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2 знаков
-// 7=>07, 1=>01, 12=>12
-pad(value) {
-  return String(value).padStart(2, "0");
+    let convertTime = convertMs(time);
+
+    refs.days.textContent = convertTime.days;
+    refs.hours.textContent = convertTime.hours;
+    refs.minutes.textContent = convertTime.minutes;
+    refs.seconds.textContent = convertTime.seconds;
+
+    refs.startBtn.addEventListener('click', startCounter)
+
+    function startCounter() {
+        const intervalId = setInterval(() => {
+            time -= 1000;
+           convertTime = convertMs(time);
+            refs.days.textContent = convertTime.days;
+            refs.hours.textContent = convertTime.hours;
+            refs.minutes.textContent = convertTime.minutes;
+            refs.seconds.textContent = convertTime.seconds;
+            if (time < 1000) {
+                clearInterval(intervalId);
+            }
+        }, 1000);
+
+        refs.startBtn.removeEventListener('click', startCounter)
+        
+    }
+    refs.choosedDate.style.display = 'none';
+    
 }
 
-updateClockface({days, hours, mins, secs}) {
-  
-  this.refs.days.textContent = `${days}`;
-  this.refs.hours.textContent = `${hours}`;
-  this.refs.mins.textContent = `${mins}`;
-  this.refs.secs.textContent = `${secs}`;
-}
-};
 
-const timer = new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date("Jul 17, 2021")
-});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
